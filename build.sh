37b426e0ce8f05abf2cfb306a47de7658ad1617a
Mirco <mcailotto96@gmail.com>
2017-12-07 20:18:23 +0100

    Fix travis

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd 'Esterni - ENG'
+cd AnalisiDeiRequisiti
+pdflatex -interaction=nonstopmode -halt-on-error AnalisiDeiRequisiti.tex
+cd ..
+cd ..
+
+cd 'LetteraDiPresentazione - ITA'
+pdflatex -interaction=nonstopmode -halt-on-error LetteraDiPresentazione.tex
+cd ..
+

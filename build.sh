3696a5f076ad62884024e42eff3f307597de325b
Mirco <mcailotto96@gmail.com>
2017-12-13 20:58:10 +0100

    Fix Travis

--- build.sh
+++ build.sh
@@ -1,24 +1,6 @@
 cd 'Esterni - ENG'
-cd AnalisiDeiRequisiti
-pdflatex -interaction=nonstopmode -halt-on-error AnalisiDeiRequisiti.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
-cd Glossario
-pdflatex -interaction=nonstopmode -halt-on-error GlossarioEST.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
 cd PianoDiProgetto
 pdflatex -interaction=nonstopmode -halt-on-error PianoDiProgetto.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
-cd PianoDiQualifica
-pdflatex -interaction=nonstopmode -halt-on-error PianoDiQualifica.tex
 cd ..
 cd ..
 
@@ -31,12 +13,6 @@ cd ..
 cd 'Interni - ITA'
 cd StudioDiFattibilità
 pdflatex -interaction=nonstopmode -halt-on-error StudioDiFattibilità.tex
-cd ..
-cd ..
-
-cd 'Interni - ITA'
-cd Verbale171122
-pdflatex -interaction=nonstopmode -halt-on-error Verbale171122.tex
 cd ..
 cd ..
 

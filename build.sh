93bcd277269940b6b9f417bf5844c70c60f0d5dd
Mirco <mcailotto96@gmail.com>
2017-12-13 20:59:43 +0100

    Fix Travis

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 cd 'Esterni - ENG'
-cd AnalisiDeiRequisiti
-pdflatex -interaction=nonstopmode -halt-on-error AnalisiDeiRequisiti.tex && echo "ok" || exit  1
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
 cd Glossario
 pdflatex -interaction=nonstopmode -halt-on-error GlossarioEST.tex && echo "ok" || exit  2
 cd ..
@@ -13,12 +7,6 @@ cd ..
 cd 'Esterni - ENG'
 cd PianoDiProgetto
 pdflatex -interaction=nonstopmode -halt-on-error PianoDiProgetto.tex && echo "ok" || exit  3
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
-cd PianoDiQualifica
-pdflatex -interaction=nonstopmode -halt-on-error PianoDiQualifica.tex && echo "ok" || exit  4
 cd ..
 cd ..
 
@@ -33,17 +21,6 @@ cd StudioDiFattibilità
 pdflatex -interaction=nonstopmode -halt-on-error StudioDiFattibilità.tex && echo "ok" || exit  6
 cd ..
 cd ..
-
-cd 'Interni - ITA'
-cd Verbale171122
-pdflatex -interaction=nonstopmode -halt-on-error Verbale171122.tex && echo "ok" || exit  7
-cd ..
-cd ..
-
-cd 'LetteraDiPresentazione - ITA'
-pdflatex -interaction=nonstopmode -halt-on-error LetteraDiPresentazione.tex && echo "ok" || exit  8
-cd ..
-
 
 # 'Esterni - ENG/AnalisiDeiRequisiti.pdf'
 # 'Esterni - ENG/GlossarioEST.pdf'

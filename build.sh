072bf034bfae9d5ed7209aa64336f0d445f0bc83
gmarraffa <marraffa3ai@gmail.com>
2017-12-16 17:30:48 +0100

    Tenuto solo il nuovo file di build e aggiornato conseguentemente i lfile di travis

--- build.sh
+++ build.sh
@@ -1,32 +1,54 @@
-cd 'Esterni - ENG'
-cd Glossario
-pdflatex -interaction=nonstopmode -halt-on-error GlossarioEST.tex && echo "ok" || exit  2
-cd ..
-cd ..
+#!/bin/bash
+<<USAGE
+  FUNZIONANTE SU DISTRIBUTZIONI LINUX
+  2 Modi per usarlo:
+  1)/bin/bash build.sh
+  2)
+    Dare permessi di esecuzione al file "chmod +x build.sh"
+    ./build.sh
+USAGE
+<<FUNZIONAMENTO
+  Esegue la compilazione per vedere se le modifiche manuali non rompono niente
+  Crea una cartella di backup dove salvare il contenuto attuale
+  Esegue l'aggiornamento del glossario
+  Compila per vedere se l'aggiornamento del glossario ha rotto qualcosa
+    Se non ha rotto niente elmina la cartella di bakcup
+    Altrimenti recupera il contenuto della cartella di backup e stampa gli errori in un log
+  FINE
+FUNZIONAMENTO
+<<NOTE
+  "&>/dev/null" --> non stampa l'output, vedere se compatibile con travis/windows
+NOTE
 
-cd 'Esterni - ENG'
-cd PianoDiProgetto
-pdflatex -interaction=nonstopmode -halt-on-error PianoDiProgetto.tex && echo "ok" || exit  3
-cd ..
-cd ..
+#Configurazione
+BACKUPDIR="/tmp/backup" #cartella di backup, verificare supporto travis/windows
+rm -r $BACKUPDIR/ &>/dev/null
 
-cd 'Interni - ITA'
-cd NormeDiProgetto
-pdflatex -interaction=nonstopmode -halt-on-error NormeDiProgetto.tex && echo "ok" || exit  5
-cd ..
-cd ..
+#prima compilazione
+COMPILELOG=$(/bin/bash compile.sh) #Prova a compilare salvandosi il risultato
+RESULT="${COMPILELOG##*$'\n'}" #Si trova l'ultima riga dell'output di compilazione
+if [[ $RESULT != "ok" ]]; then
+  echo "LE MODIFCHE EFFETTUATE SONO ERRATE, CONTROLLARE I FILE MODIFICATI"
+  exit 10001;
+fi
 
-cd 'Interni - ITA'
-cd StudioDiFattibilità
-pdflatex -interaction=nonstopmode -halt-on-error StudioDiFattibilità.tex && echo "ok" || exit  6
-cd ..
-cd ..
-
-# 'Esterni - ENG/AnalisiDeiRequisiti.pdf'
-# 'Esterni - ENG/GlossarioEST.pdf'
-# 'Esterni - ENG/PianoDiProgetto.pdf'
-# 'Esterni - ENG/PianoDiQualifica.pdf'
-# 'Interni - ITA/NormeDiProgetto.pdf'
-# 'Interni - ITA/StudioDiFattibilità.pdf'
-# 'Interni - ITA/Verbale171122.pdf'
-# 'LetteraDiPresentazione - ITA/LetteraDiPresentazione.pdf'
+#UPDATE DEL GLOSSARIO
+mkdir $BACKUPDIR/ &>/dev/null #Crea la cartella di backup
+cp -r * $BACKUPDIR/ &>/dev/null #Copia il contenuto corrente nella cartella di backup
+/bin/bash gupdate.sh #Esegue l'update dei termini del glossario
+COMPILELOG=$(/bin/bash compile.sh) #Prova a compilare salvandosi il risultato
+RESULT="${COMPILELOG##*$'\n'}" #Si trova l'ultima riga dell'output di compilazione
+if [[ $RESULT == 'ok' ]]; then
+  rm -r $BACKUPDIR #Rimuove la cartella di backup
+  echo "EVERYTHING IS GONNA BE ALL RIGHT"
+  echo "ok"
+  exit 0;
+else
+  echo "$COMPILELOG" > compile_errors.log
+  rm -r * #Elimina il contenuto rotto da gupdate.sh
+  mv -f $BACKUPDIR/* . #Recupera la cartella di backup
+  rm -r $BACKUPDIR/ #Rimuove la cartella di bakcup
+  echo "GLOSSARY UPDATE BROKE SOMETHING, ROLLBACK EXECUTED"
+  echo "See compile_errors.log for further details"
+  exit 10000;
+fi

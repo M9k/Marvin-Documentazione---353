82bac351f6545c441553beba39298eaf4e1d8fa3
gmarraffa <marraffa3ai@gmail.com>
2017-12-16 15:56:25 +0100

    Aggiunto script aggiornamento termini del glossario e nuova build che comprende il procedimento

---
+++ compile.sh
@@ -0,0 +1,41 @@
+<<USAGE
+  FUNZIONANTE SU DISTRIBUTZIONI LINUX
+  2 Modi per usarlo:
+  1)/bin/bash compile.sh
+  2)
+    Dare permessi di esecuzione al file "chmod +x build.sh"
+    ./compile.sh
+USAGE
+
+cd 'Esterni - ENG'
+cd Glossario
+pdflatex -interaction=nonstopmode -halt-on-error GlossarioEST.tex && echo "ok" || exit  2
+cd ..
+cd ..
+
+cd 'Esterni - ENG'
+cd PianoDiProgetto
+pdflatex -interaction=nonstopmode -halt-on-error PianoDiProgetto.tex && echo "ok" || exit  3
+cd ..
+cd ..
+
+cd 'Interni - ITA'
+cd NormeDiProgetto
+pdflatex -interaction=nonstopmode -halt-on-error NormeDiProgetto.tex && echo "ok" || exit  5
+cd ..
+cd ..
+
+cd 'Interni - ITA'
+cd StudioDiFattibilità
+pdflatex -interaction=nonstopmode -halt-on-error StudioDiFattibilità.tex && echo "ok" || exit  6
+cd ..
+cd ..
+
+# 'Esterni - ENG/AnalisiDeiRequisiti.pdf'
+# 'Esterni - ENG/GlossarioEST.pdf'
+# 'Esterni - ENG/PianoDiProgetto.pdf'
+# 'Esterni - ENG/PianoDiQualifica.pdf'
+# 'Interni - ITA/NormeDiProgetto.pdf'
+# 'Interni - ITA/StudioDiFattibilità.pdf'
+# 'Interni - ITA/Verbale171122.pdf'
+# 'LetteraDiPresentazione - ITA/LetteraDiPresentazione.pdf'

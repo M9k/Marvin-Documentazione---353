ffc1b78efcb4942b8c840b2d22d9903ca28cda56
Riccardo Giorato <giorat.96@gmail.com>
2017-12-17 18:07:01 +0100

    nuovo branch e scheletro PdQ

--- build.sh
+++ build.sh
@@ -1,48 +1,10 @@
 cd 'Esterni - ENG'
-cd AnalisiDeiRequisiti
-pdflatex -interaction=nonstopmode -halt-on-error AnalisiDeiRequisiti.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
-cd Glossario
-pdflatex -interaction=nonstopmode -halt-on-error GlossarioEST.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
-cd PianoDiProgetto
-pdflatex -interaction=nonstopmode -halt-on-error PianoDiProgetto.tex
-cd ..
-cd ..
-
-cd 'Esterni - ENG'
 cd PianoDiQualifica
 pdflatex -interaction=nonstopmode -halt-on-error PianoDiQualifica.tex
 cd ..
 cd ..
 
-cd 'Interni - ITA'
-cd NormeDiProgetto
-pdflatex -interaction=nonstopmode -halt-on-error NormeDiProgetto.tex
-cd ..
-cd ..
 
-cd 'Interni - ITA'
-cd StudioDiFattibilità
-pdflatex -interaction=nonstopmode -halt-on-error StudioDiFattibilità.tex
-cd ..
-cd ..
-
-cd 'Interni - ITA'
-cd Verbale171122
-pdflatex -interaction=nonstopmode -halt-on-error Verbale171122.tex
-cd ..
-cd ..
-
-cd 'LetteraDiPresentazione - ITA'
-pdflatex -interaction=nonstopmode -halt-on-error LetteraDiPresentazione.tex
-cd ..
 
 # 'Esterni - ENG/AnalisiDeiRequisiti.pdf'
 # 'Esterni - ENG/GlossarioEST.pdf'

2316593fd3540ea19a5117561067adf705680d31
Mirco <mcailotto96@gmail.com>
2018-01-08 19:11:00 +0100

    update

--- build.sh
+++ build.sh
@@ -21,33 +21,39 @@ FUNZIONAMENTO
 NOTE
 
 #Configurazione
-#BACKUPDIR="/tmp/backup" #cartella di backup, verificare supporto travis/windows
-#rm -r $BACKUPDIR/ &>/dev/null
+BACKUPDIR="/tmp/backup" #cartella di backup, verificare supporto travis/windows
+rm -r $BACKUPDIR/ &>/dev/null
 
 #prima compilazione
-#COMPILELOG=$(/bin/bash compile.sh) #Prova a compilare salvandosi il risultato
-#RESULT="${COMPILELOG##*$'\n'}" #Si trova l'ultima riga dell'output di compilazione
-#if [[ $RESULT != "ok" ]]; then
-#  echo "LE MODIFCHE EFFETTUATE SONO ERRATE, CONTROLLARE I FILE MODIFICATI"
-#  exit 10001;
-#fi
+COMPILELOG=$(/bin/bash compile.sh) #Prova a compilare salvandosi il risultato
+RESULT="${COMPILELOG##*$'\n'}" #Si trova l'ultima riga dell'output di compilazione
+if [[ $RESULT != "ok" ]]; then
+  echo "LE MODIFCHE EFFETTUATE SONO ERRATE, CONTROLLARE I FILE MODIFICATI"
+  exit 10001;
+fi
 
 #UPDATE DEL GLOSSARIO
-#mkdir $BACKUPDIR/ &>/dev/null #Crea la cartella di backup
-#cp -r * $BACKUPDIR/ &>/dev/null #Copia il contenuto corrente nella cartella di backup
-#/bin/bash gupdate.sh #Esegue l'update dei termini del glossario
+mkdir $BACKUPDIR/ &>/dev/null #Crea la cartella di backup
+cp -r * $BACKUPDIR/ &>/dev/null #Copia il contenuto corrente nella cartella di backup
+
+###########################################
+#  FUNZIONE TEMPORANEAMENTE DISABILITATA  #
+###########################################
+# /bin/bash gupdate.sh #Esegue l'update dei termini del glossario
+###########################################
+
 COMPILELOG=$(/bin/bash compile.sh) #Prova a compilare salvandosi il risultato
 RESULT="${COMPILELOG##*$'\n'}" #Si trova l'ultima riga dell'output di compilazione
 if [[ $RESULT == 'ok' ]]; then
-#  rm -r $BACKUPDIR #Rimuove la cartella di backup
+  rm -r $BACKUPDIR #Rimuove la cartella di backup
   echo "EVERYTHING IS GONNA BE ALL RIGHT"
   echo "ok"
   exit 0;
 else
   echo "$COMPILELOG" > compile_errors.log
-#  rm -r * #Elimina il contenuto rotto da gupdate.sh
-#  mv -f $BACKUPDIR/* . #Recupera la cartella di backup
-#  rm -r $BACKUPDIR/ #Rimuove la cartella di bakcup
+  rm -r * #Elimina il contenuto rotto da gupdate.sh
+  mv -f $BACKUPDIR/* . #Recupera la cartella di backup
+  rm -r $BACKUPDIR/ #Rimuove la cartella di bakcup
   echo "GLOSSARY UPDATE BROKE SOMETHING, ROLLBACK EXECUTED"
   echo "See compile_errors.log for further details"
   exit 10000;
